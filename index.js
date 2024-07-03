a7b7802504abf3a8362a839f1ce1dcc2e03dcaf7
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-07-03 05:11:17 +0300

    Add updated FE package

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, response, request, next) => {
 
   if (error.name === "CastError") {
     return response.status(404).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(404).json({ error: error.message });
   }
 
   next(error);
@@ -47,23 +49,20 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response.status(404).json({
-      error: "name or number missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -94,14 +93,13 @@ app.delete("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
